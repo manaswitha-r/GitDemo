75e6b182fccc8c79b3ac6b79ea2573131ce52100
manaswitha <manaswithareddy345@gmail.com>
2021-07-10 15:41:58 +0530

    first commit

---
+++ src/test/java/udemy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package udemy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void MobileApplication()
+	{
+		System.out.println("MobileApplication");	
+	}
+	
+	@Test
+	public void Apps()
+	{
+		System.out.println("Apps");	
+		
+	}
+
+}
---
+++ src/test/java/udemy/Mavenjava/RestAPITest.java
@@ -0,0 +1,19 @@
+package udemy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void UserFriendly()
+	{
+	System.out.println("UserFriendly");	
+	}
+	
+	@Test
+	public void SystemFriendly()
+	{
+		System.out.println("SystemFriendly");
+	}
+
+}
